350cd715398998cb882f947bb7b48ae8f661be43
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-04-25 04:27:08 -0300

    Start Project

---
+++ src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;   
+    width: 100%;
+    max-width: 750px;
+    margin: auto;    
+    padding: 50px 0;
+    
+`;
+export const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: auto;
+`;
+export const LogoLink = styled.a`
+    display: block;
+`;
+export const InfoArea = styled.div`
+
+`;
+export const GridArea = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+`;
