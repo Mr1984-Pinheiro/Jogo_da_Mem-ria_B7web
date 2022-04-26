23b1a1637487b13d9420bc51be01d532a49e349b
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-04-26 02:45:30 -0300

    Creating the InfoItem component

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -6,21 +6,42 @@ export const Container = styled.div`
     max-width: 750px;
     margin: auto;    
     padding: 50px 0;
+
+    @media (max-width: 750px) {
+        flex-direction: column;
+    }
     
 `;
 export const Info = styled.div`
     display: flex;
     flex-direction: column;
     width: auto;
+
+    @media (max-width: 750px){
+        align-items: center;
+        margin-bottom: 50px;
+    }
 `;
 export const LogoLink = styled.a`
     display: block;
 `;
 export const InfoArea = styled.div`
+    width: 100%;
+    margin: 10px 0;
 
+    @media (max-width: 750px) {
+        display: flex;
+        justify-content: space-around;
+        text-align: center;
+    }
 `;
 export const GridArea = styled.div`
     flex: 1;
     display: flex;
-    justify-content: flex-end;
+    justify-content: flex-end;    
+
+    @media (max-width: 750px) {
+        justify-content: center;
+        margin: 0 20px;
+    }
 `;
